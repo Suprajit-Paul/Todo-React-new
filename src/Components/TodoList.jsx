f8e76db36f5dc28acd67014fcd6f6e2c0a19da8e
suprajit <suprajitpaul18@gmail.com>
2025-11-13 18:32:36 +0530

    Update

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TodoList = ({todos}) => {
+const TodoList = ({todos, delToDo}) => {
   return (
     <ul>
       <ul className="max-w-lg mx-auto mt-6 space-y-3"></ul>
@@ -9,6 +9,7 @@ const TodoList = ({todos}) => {
             className="bg-white px-4 py-3 rounded-lg shadow-sm flex justify-between items-center hover:shadow-md transition-shadow border border-gray-100"
           >
             <span className="text-gray-800">{item.text}</span>
+            <button onClick={() => delToDo(item.id)}>DEL</button>
           </li>
         ))}
     </ul>
